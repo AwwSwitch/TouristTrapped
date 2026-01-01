9666f2f27533071bba0854d3e2e466362573784c
Nigel Phang <nigelprankster5@gmail.com>
2026-01-01 10:09:34 +0800

    improved weapons files readability :)

---
+++ weapons/axe.java
@@ -0,0 +1,17 @@
+package weapons;
+
+public class axe {
+    private double damage;
+    private double durability;
+    private double attackspeed;
+
+    axe(double damage, double durability, double attackspeed){
+        this.damage = damage;
+        this.durability = durability;
+        this.attackspeed = attackspeed;
+    }
+
+    void display_stats(){
+        System.out.printf("cur damage is %.2f, durability is %.2f, attackspeed is %.2f", damage, durability, attackspeed);
+    }
+}
